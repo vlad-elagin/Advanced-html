e784d48f99651a5bdff52a3d38eab20bab1e2516
Vlad Elagin <vlad.elagin@keenethics.com>
2018-04-10 21:09:42 +0300

    pug added, gulpfile updated, side menu wip

--- gulpfile.js
+++ gulpfile.js
@@ -6,45 +6,38 @@ var clean =         require('gulp-clean-css');
 var browserSync =   require('browser-sync').create();
 var autoprefixer =  require('gulp-autoprefixer');
 var notify =        require('gulp-notify');
+var pug =           require('gulp-pug');
+var injectSVG =     require('gulp-inject-svg');
 
-
-// таск для обработки стилей
 gulp.task('styles', function() {
-	// берем основной файл стилей, в который импортятся все остальные
   gulp.src(['styles/general.styl'])
-  	// превращаем less в чистый css
     .pipe(stylus())
-    // добавляем сообщение об ошибке
     .on("error", notify.onError())
-    // меняем имя
     .pipe(rename('bundle.css'))
-    // добавляем вендорные префиксы
     .pipe(autoprefixer({
         browsers: ['last 2 versions'],
         cascade: false
     }))
-    // сжимаем стили, чистим от комментариев
     .pipe(clean())
-    // записываем изменения в папку dist
     .pipe(gulp.dest('./', { overwrite: true }))
-    // перезагружаем страницу в браузере
     .pipe(browserSync.stream());
 });
 
-// таск для обработки разметки
-// сейчас он просто переносит файлы из src в dist
-gulp.task('html', function() {
-	gulp.src(['./*.html'])
-		.pipe(gulp.dest('./'));
+gulp.task('views', function() {
+	gulp.src(['./views/index.pug'])
+    .pipe(pug())
+    .pipe(injectSVG())
+    .pipe(rename('index.html'))
+		.pipe(gulp.dest('./', { overwrite: true }));
   browserSync.reload();
 });
 
 // дефолтный таск
-gulp.task('default', ['styles', 'html'], function() {
+gulp.task('default', ['styles', 'views'], function() {
 	// следим за изменениями в файлах стилей и запускаем таск обработки
   gulp.watch('styles/**/*.styl', ['styles']);
 	// то же самое для html
-	gulp.watch('./*.html', ['html']);
+	gulp.watch('./views/**/*.pug', ['views']);
 
 	// сервер для разработки
 	browserSync.init({
