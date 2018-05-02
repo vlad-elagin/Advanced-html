0f86f446ed5e79a02c4ba113f41e15ce73ec26b9
Vlad Elagin <vlad.elagin@keenethics.com>
2018-05-03 02:27:00 +0300

    slider wip

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
+// toggling menu
 var menuButton = document.querySelector('.menu-link');
 var menu = document.querySelector('nav.menu');
 
@@ -5,3 +6,37 @@ menuButton.addEventListener('click', function(e) {
   e.preventDefault();
   menu.classList.toggle('opened');
 });
+
+// smoothies slider
+var leftBtn = document.querySelector('.featured-products .arrow.left');
+var rightBtn = document.querySelector('.featured-products .arrow.right');
+var counter = document.querySelector('.featured-products .slider-controls span');
+var current = 1;
+
+counter.textContent = current;
+
+function pickSmoothie(index) {
+  document.querySelector('.featured-products .product-wrapper.active').classList.remove('active');
+  var smoothie = document.querySelectorAll('.featured-products .product-wrapper')[index];
+  smoothie.classList.add('active');
+
+  var wrapper = document.querySelector('.featured-products .products');
+  wrapper.style.left = (index + 1) * 340 + 'px';
+
+  current = index;
+  counter.textContent = current;
+}
+
+leftBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  if (current - 1 === -1) return;
+  console.log('picking ' + (current - 1) + ' smoothie');
+  pickSmoothie(current - 1);
+})
+
+rightBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  if (current + 1 === 3) return;
+  console.log('picking ' + (current + 1) + ' smoothie');
+  pickSmoothie(current + 1);
+})
