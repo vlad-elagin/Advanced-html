77a555dfa15a59587636a642ef13d96e7d692460
Vlad Elagin <vlad.elagin@keenethics.com>
2018-05-21 07:08:19 +0300

    boost section almost ready

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,7 +13,7 @@ var rightBtn = document.querySelector('.featured-products .arrow.right');
 var counter = document.querySelector('.featured-products .slider-controls span');
 var current = 1;
 
-counter.textContent = current;
+counter.textContent = current + 1;
 
 function pickSmoothie(index) {
   document.querySelector('.featured-products .product-wrapper.active').classList.remove('active');
@@ -26,19 +26,17 @@ function pickSmoothie(index) {
   wrapper.style.transform = 'translateX(' + positions[index] + 'px)';
 
   current = index;
-  counter.textContent = current;
+  counter.textContent = current + 1;
 }
 
 leftBtn.addEventListener('click', function(e) {
   e.preventDefault();
   if (current - 1 === -1) return;
-  console.log('picking ' + (current - 1) + ' smoothie');
   pickSmoothie(current - 1);
 })
 
 rightBtn.addEventListener('click', function(e) {
   e.preventDefault();
   if (current + 1 === 3) return;
-  console.log('picking ' + (current + 1) + ' smoothie');
   pickSmoothie(current + 1);
 })
